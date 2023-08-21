4c1a41748fe4092fa090877acefe50c0632c365b
Hukiro <123516825+Hukiro@users.noreply.github.com>
2023-08-21 22:06:24 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,44 @@
+import telebot
+
+from config import keys, TOKEN
+from extensions import APIException, Convertor
+
+bot = telebot.TeleBot(TOKEN)
+
+
+@bot.message_handler(commands=['start', 'help'])
+def start(message: telebot.types.Message):
+    text = 'Для начала работы введите комманду в следующем формате:\n' \
+           '<Наименование валюты>' \
+           '<В какую валюту перевести> ' \
+           '<Количество переводимой валюты>\n' \
+           'Перечень доступных валют: /values\n' \
+           'Пример правильного запроса: /example'
+    bot.reply_to(message, text)
+
+
+@bot.message_handler(commands=['values'])
+def start(message: telebot.types.Message):
+    text = 'Доступные валюты: '
+    for key in keys.keys():
+        text = '\n'.join((text, key, ))
+    bot.reply_to(message, text)
+
+
+@bot.message_handler(commands=['example'])
+def start(message: telebot.types.Message):
+    text = '<Что><Куда><Сколько>\nбиткоин эфириум 0.1'
+    bot.reply_to(message, text)
+
+
+@bot.message_handler(content_types=['text', ])
+def convert(message: telebot.types.Message):
+    try:
+        bot.reply_to(message, Convertor.get_price(message.text.split(' ')))
+    except APIException as e:
+        bot.reply_to(message, f'Ошибка пользователя.\n{e}')
+    except Exception as e:
+        bot.reply_to(message, f'Не удалось обработать команду.\n{e}')
+
+
+bot.polling()
